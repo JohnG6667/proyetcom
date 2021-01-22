be555f563dbfde8ddda3c28ebe1f87fc85d32f31
JohnG6667 <60703340+JohnG6667@users.noreply.github.com>
2021-01-22 17:16:21 -0500

    Moví resolver

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+import resolvers from "./resolver";
 import { GraphQLServer } from "graphql-yoga";
-import { mergeTypeDefs } from '@graphql-tools/merge';
-import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeTypeDefs } from "@graphql-tools/merge";
+import { loadFilesSync } from "@graphql-tools/load-files";
 
 const typeDefs = mergeTypeDefs(loadFilesSync(`${__dirname}/**/*.graphql`));
-import resolvers from "./resolver"
+
 const server = new GraphQLServer({
   typeDefs,
   resolvers
 });
 
-
 server.start({ port: 3100 }, ({ port }) => {
-  console.log('Server on port http://localhost:'+port);
+  console.log("Server on port http://localhost:" + port);
 });
 
 /*//Variables
